5206d011bd018823f3397aecc9ca6e2054872efc
Nicole <Nicole.Ahima@gmail.com>
2015-12-02 17:42:29 -0600

    First Commit

---
+++ Check-In/Models/Teachers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Check_In.Models
+{
+    public class Teachers
+    {
+            [Key]
+            public int TeacherId { get; set; }
+            public string Name { get; set; }
+            public ApplicationUser Charlene { get; set; }
+
+       
+
+        }
+    }
