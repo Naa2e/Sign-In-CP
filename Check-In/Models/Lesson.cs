7f9786bcf9d1c4154d4301d2e5e3cac5fed1c2ac
Nicole <Nicole.Ahima@gmail.com>
2015-12-07 20:12:35 -0600

    Added Teacher Repo and Updated Lesson Class with one to many relationships

--- Check-In/Models/Lesson.cs
+++ Check-In/Models/Lesson.cs
@@ -14,10 +14,13 @@ namespace Check_In.Models
         public string Title { get; set; }
         public enum DayOfWeek { } 
 
+        public virtual List<Student> Class { get; set; }
+        public virtual List<Payment> Revenue { get; set; }
+
         public Lesson()
         {
-            Student Student = new Student();
-            Payment Payment = new Payment();
+            Class = new List<Student>();
+            Revenue = new List<Payment>();
         }
     }
 }
